37b43e72c02b38b43b198af87d955b8b16ddf24a
Didrik Nathaniel LLoyd Aasland Skjelbred <141577441+DidrikGetacademy@users.noreply.github.com>
2025-05-04 15:03:34 +0200

    update

--- download_local_model.py
+++ download_local_model.py
@@ -1,27 +1,11 @@
 from huggingface_hub import snapshot_download
 import os
 
-def download_Qwen_Coder_7B_Instruct():
-    model_repo = "Qwen/Qwen2.5-Coder-7B-Instruct"
-    local_dir = "./local_model/qwen_coder_7b_instruct"
-
-    if not os.path.exists(local_dir):
-        print(f"📥 Downloading {model_repo} into {local_dir}...")
-        snapshot_download(
-            repo_id=model_repo,
-            local_dir=local_dir,
-            resume_download=True,
-            local_dir_use_symlinks=False  
-        )
-        print(f"✅ Qwen2.5-Coder-7B-Instruct downloaded successfully.")
-    else:
-        print(f"✅ Model already exists at {local_dir}, skipping download.")
-
 
 
 def download_deepseek_coder_7b_instruct():
     model_repo = "deepseek-ai/deepseek-coder-6.7b-instruct"
-    local_dir = "./local_model/deepseek_coder_7b_instruct"
+    local_dir = "./local_model/deepseek-coder-6.7b-instruct"
 
     if not os.path.exists(local_dir):
         print(f"📥 Downloading {model_repo} into {local_dir}...")
@@ -31,9 +15,12 @@ def download_deepseek_coder_7b_instruct():
             resume_download=True,
             local_dir_use_symlinks=False
         )
-        print(f"✅ Deepseek-Coder-6.7B-Instruct downloaded successfully.")
+        print(f"✅ deepseek-coder-6.7b-instruct downloaded successfully.")
     else:
         print(f"✅ Model already exists at {local_dir}, skipping download.")
+
+
+
 
 
 def download_Mistral_7B_Instruct_v02():
@@ -53,25 +40,11 @@ def download_Mistral_7B_Instruct_v02():
         print(f"✅ Model already exists at {local_dir}, skipping download.")
 
 
-def download_Qwen_Coder_Instruct_3b():
-    model_repo = "Qwen/Qwen2.5-VL-3B-Instruct"
-    local_dir = "./local_model/Qwen2.5-Coder-3B-Instruct"
 
-    if not os.path.exists(local_dir):
-        print(f"📥 Downloading {model_repo} into {local_dir}...")
-        snapshot_download(
-            repo_id=model_repo,
-            local_dir=local_dir,
-            resume_download=True,
-            local_dir_use_symlinks=False
-        )
-        print(f"✅ Qwen2.5-Coder-3B-Instruct downloaded successfully.")
-    else:
-        print(f"✅ Model already exists at {local_dir}, skipping download.")
 
-def download_Qwen_Coder_Instruct_5b():
+def download_Qwen_Coder2_5_Instruct_1_5b():
     model_repo = "Qwen/Qwen2.5-1.5B-Instruct"
-    local_dir = "./local_model/Qwen2.5-Coder-5B-Instruct"
+    local_dir = "./local_model/Qwen2.5-1.5B-Instruct"
 
     if not os.path.exists(local_dir):
         print(f"📥 Downloading {model_repo} into {local_dir}...")
@@ -81,13 +54,16 @@ def download_Qwen_Coder_Instruct_5b():
             resume_download=True,
             local_dir_use_symlinks=False
         )
-        print(f"✅ Qwen2.5-Coder-3B-Instruct downloaded successfully.")
+        print(f"✅ Qwen2.5-1.5B-Instruct downloaded successfully.")
     else:
         print(f"✅ Model already exists at {local_dir}, skipping download.")
 
-def download_Qwen__Instruct_7b():
+
+
+
+def download_Qwen_7b_Instruct_1M():
     model_repo = "Qwen/Qwen2.5-7B-Instruct-1M"
-    local_dir = "./local_model/Qwen2.5-Coder-7B-Instruct"
+    local_dir = "./local_model/Qwen2.5-7B-Instruct-1M"
 
     if not os.path.exists(local_dir):
         print(f"📥 Downloading {model_repo} into {local_dir}...")
@@ -97,12 +73,15 @@ def download_Qwen__Instruct_7b():
             resume_download=True,
             local_dir_use_symlinks=False
         )
-        print(f"✅ Qwen2.5-Coder-7B-Instruct downloaded successfully.")
+        print(f"✅ Qwen2.5-7B-Instruct-1M downloaded successfully.")
     else:
         print(f"✅ Model already exists at {local_dir}, skipping download.")
 
 
-def download_Qwen_Coder_Instruct_7bb():
+
+
+
+def download_Qwen_Coder_Instruct_7b():
     model_repo = "Qwen/Qwen2.5-Coder-7B-Instruct"
     local_dir = "./local_model/Qwen/Qwen2.5-Coder-7B-Instruct"
 
@@ -117,6 +96,10 @@ def download_Qwen_Coder_Instruct_7bb():
         print(f"✅ Qwen2.5-Coder-7B-Instruct downloaded successfully.")
     else:
         print(f"✅ Model already exists at {local_dir}, skipping download.")
+
+
+
+
 
 
 def download_Qwen2_5_Coder_3B_Instruct():
@@ -135,7 +118,11 @@ def download_Qwen2_5_Coder_3B_Instruct():
     else:
         print(f"✅ Model already exists at {local_dir}, skipping download.")
 
-def download_microsoft():
+
+
+
+
+def download_microsoft_Phi_3_mini_128k_instruct():
     model_repo = "microsoft/Phi-3-mini-128k-instruct"
     local_dir = "./local_model/microsoft/microsoft/Phi-3-mini-128k-instruct"
 
@@ -155,5 +142,55 @@ def download_microsoft():
 
 
 
+def download_microsoft_Phi_4_mini_instruct():
+    model_repo = "microsoft/Phi-4-mini-instruct "
+    local_dir = "./local_model/microsoft/microsoft/Phi-4-mini-instruct "
+    if not os.path.exists(local_dir):
+        print(f"📥 Downloading {model_repo} into {local_dir}...")
+        snapshot_download(
+            repo_id=model_repo,
+            local_dir=local_dir,
+            resume_download=True,
+            local_dir_use_symlinks=False
+        )
+        print(f"✅ microsoft/microsoft/Phi-4-mini-instruct downloaded successfully.")
+    else:
+        print(f"✅ Model already exists at {local_dir}, skipping download.")
+
+
+
+def download_microsoft_Phi_4_multimodal_instruct():
+    model_repo = "microsoft/Phi-4-multimodal-instruct"
+    local_dir = "./local_model/microsoft/microsoft/Phi-4-multimodal-instruct"
+    if not os.path.exists(local_dir):
+        print(f"📥 Downloading {model_repo} into {local_dir}...")
+        snapshot_download(
+            repo_id=model_repo,
+            local_dir=local_dir,
+            resume_download=True,
+            local_dir_use_symlinks=False
+        )
+        print(f"✅ microsoft/Phi-4-multimodal-instruct downloaded successfully.")
+    else:
+        print(f"✅ Model already exists at {local_dir}, skipping download.")
+
+
+def download_microsoft_microsoft_Phi_4_reasoning_plus():
+    model_repo = "microsoft/Phi-4-reasoning-plus"
+    local_dir = "./local_model/microsoft/microsoft/microsoft/Phi-4-reasoning-plus"
+    if not os.path.exists(local_dir):
+        print(f"📥 Downloading {model_repo} into {local_dir}...")
+        snapshot_download(
+            repo_id=model_repo,
+            local_dir=local_dir,
+            resume_download=True,
+            local_dir_use_symlinks=False
+        )
+        print(f"✅ microsoft/Phi-4-reasoning-plus downloaded successfully.")
+    else:
+        print(f"✅ Model already exists at {local_dir}, skipping download.")
+
+
+
+
 if __name__ == "__main__":
-     download_microsoft()
