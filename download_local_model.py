7c1d0b6c0b03196c6b1d3b8b56626d5eba433f34
Didrik Nathaniel LLoyd Aasland Skjelbred <141577441+DidrikGetacademy@users.noreply.github.com>
2025-05-04 15:18:10 +0200

    update

--- download_local_model.py
+++ download_local_model.py
@@ -4,6 +4,14 @@ import os
 
 
 def download_deepseek_coder_7b_instruct():
+    """
+    Model Features ----> 
+    ------------------------------------------------------------------------
+    
+    Ideal Use Cases: 
+    ------------------------------------------------------------------------
+    
+    """
     model_repo = "deepseek-ai/deepseek-coder-6.7b-instruct"
     local_dir = "./local_model/deepseek-coder-6.7b-instruct"
 
@@ -24,6 +32,14 @@ def download_deepseek_coder_7b_instruct():
 
 
 def download_Mistral_7B_Instruct_v02():
+    """
+    Model Features ----> 
+    ------------------------------------------------------------------------
+    
+    Ideal Use Cases: 
+    ------------------------------------------------------------------------
+    
+    """
     model_repo = "Mistral-7B-Instruct-v0.2"
     local_dir = "./local_model/Mistral-7B-Instruct-v0.2"
 
@@ -43,6 +59,14 @@ def download_Mistral_7B_Instruct_v02():
 
 
 def download_Qwen_Coder2_5_Instruct_1_5b():
+    """
+    Model Features ----> 
+    ------------------------------------------------------------------------
+    
+    Ideal Use Cases: 
+    ------------------------------------------------------------------------
+    
+    """
     model_repo = "Qwen/Qwen2.5-1.5B-Instruct"
     local_dir = "./local_model/Qwen2.5-1.5B-Instruct"
 
@@ -62,6 +86,14 @@ def download_Qwen_Coder2_5_Instruct_1_5b():
 
 
 def download_Qwen_7b_Instruct_1M():
+    """
+    Model Features ----> 
+    ------------------------------------------------------------------------
+    
+    Ideal Use Cases: 
+    ------------------------------------------------------------------------
+    
+    """
     model_repo = "Qwen/Qwen2.5-7B-Instruct-1M"
     local_dir = "./local_model/Qwen2.5-7B-Instruct-1M"
 
@@ -82,6 +114,14 @@ def download_Qwen_7b_Instruct_1M():
 
 
 def download_Qwen_Coder_Instruct_7b():
+    """
+    Model Features ----> 
+    ------------------------------------------------------------------------
+    
+    Ideal Use Cases: 
+    ------------------------------------------------------------------------
+    
+    """
     model_repo = "Qwen/Qwen2.5-Coder-7B-Instruct"
     local_dir = "./local_model/Qwen/Qwen2.5-Coder-7B-Instruct"
 
@@ -103,6 +143,15 @@ def download_Qwen_Coder_Instruct_7b():
 
 
 def download_Qwen2_5_Coder_3B_Instruct():
+    """
+    Model Features ----> 
+    ------------------------------------------------------------------------
+
+    
+    Ideal Use Cases: 
+    ------------------------------------------------------------------------
+    
+    """
     model_repo = "Qwen/Qwen2.5-Coder-3B-Instruct"
     local_dir = "./local_model/Qwen/Qwen2.5-Coder-3B-Instruct"
 
@@ -123,6 +172,15 @@ def download_Qwen2_5_Coder_3B_Instruct():
 
 
 def download_microsoft_Phi_3_mini_128k_instruct():
+    """
+    Model Features ----> 
+    ------------------------------------------------------------------------
+    
+    Ideal Use Cases: 
+    ------------------------------------------------------------------------
+
+
+    """
     model_repo = "microsoft/Phi-3-mini-128k-instruct"
     local_dir = "./local_model/microsoft/microsoft/Phi-3-mini-128k-instruct"
 
@@ -143,6 +201,21 @@ def download_microsoft_Phi_3_mini_128k_instruct():
 
 
 def download_microsoft_Phi_4_mini_instruct():
+    """ A lightweight, efficient model optimized for environments with limited computational resources.
+     Model Features ----> 
+    ------------------------------------------------------------------------
+    -Designed for memory and compute-constrained environments.
+    -Supports a 128K token context length.
+    -Trained on synthetic and filtered public data, focusing on high-quality, reasoning-dense content.
+    -Incorporates supervised fine-tuning and direct preference optimization for precise instruction adherence and robust safety measures.
+
+    Ideal Use Cases: 
+    ------------------------------------------------------------------------
+    -Mobile applications, 
+    -edge devices,
+    -scenarios where low latency and efficient performance are critical
+    
+    """
     model_repo = "microsoft/Phi-4-mini-instruct "
     local_dir = "./local_model/microsoft/microsoft/Phi-4-mini-instruct "
     if not os.path.exists(local_dir):
@@ -160,6 +233,23 @@ def download_microsoft_Phi_4_mini_instruct():
 
 
 def download_microsoft_Phi_4_multimodal_instruct():
+    """A versatile model capable of processing and understanding multiple modalities, including text, images, and audio.
+     Model Features ---> Handles text, image, and audio inputs, generating text outputs.
+    ------------------------------------------------------------------------
+    -Supports a 128K token context length.
+    -Text: Supports 24 languages
+    -Vision: Primarily English.
+    -Audio: English, Chinese, German, French, Italian, Japanese, Spanish, Portuguese.
+
+    
+    Ideal Use Cases: 
+    ------------------------------------------------------------------------
+    Applications requiring multimodal understanding, such as 
+    virtual assistants that interpret 
+    -images 
+    -audio,
+    -tools that analyze documents combining text and visuals
+    """
     model_repo = "microsoft/Phi-4-multimodal-instruct"
     local_dir = "./local_model/microsoft/microsoft/Phi-4-multimodal-instruct"
     if not os.path.exists(local_dir):
@@ -175,7 +265,23 @@ def download_microsoft_Phi_4_multimodal_instruct():
         print(f"✅ Model already exists at {local_dir}, skipping download.")
 
 
+
+
 def download_microsoft_microsoft_Phi_4_reasoning_plus():
+    """A model fine-tuned for advanced reasoning tasks, particularly in mathematics, science, and coding.
+    Model Features --->
+    --------------------------------------------------------------------------------------
+    -Built upon the base Phi-4 model with 14 billion parameters.
+    -Supports a 32K token context length, extendable up to 64K for complex tasks.
+    -Fine-tuned using supervised learning on chain-of-thought datasets and reinforcement learning for enhanced reasoning capabilities.
+    -Generates detailed reasoning steps followed by concise summaries.
+    
+    Ideal Use Cases: 
+    --------------------------------------------------------------------------------------
+    -Tasks requiring deep analytical thinking, such as complex problem-solving in mathematics, 
+    -cientific research
+    -advanced coding challenges
+    """
     model_repo = "microsoft/Phi-4-reasoning-plus"
     local_dir = "./local_model/microsoft/microsoft/microsoft/Phi-4-reasoning-plus"
     if not os.path.exists(local_dir):
